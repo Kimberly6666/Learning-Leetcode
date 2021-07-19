27e7aee31e1af9996c8212b60f77e7c45f6332fc
Kimberly6666 <44635001+Kimberly6666@users.noreply.github.com>
2021-07-19 21:10:55 +0800

    Two pointers

---
+++ Linked list/141. Linked List Cycle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+"""
+FileName: 141. Linked List Cycle.py
+Author: Kimberly Gao
+Date: 2021/07/15 9:10 PM
+Version: 0.1
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if head == None and head.next == None:
+            return False
+        slow, fast = head, head
+        while(fast != None and fast.next != None):
+            temp = head
+            if temp == fast:
+                return True
+            while(True):
+                fast = fast.next
+                temp = temp.next
+                if fast == temp:
+                    break
+            return True
+        return False
